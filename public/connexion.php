8dff2a7b27ec7885d95afe47cf04c5163c2c3282
jsculteur <julien.sculteur@pobi.fr>
2021-10-19 11:34:23 +0200

    fix: Formulaire connexion et user agent

--- public/connexion.php
+++ public/connexion.php
@@ -1,6 +1,6 @@
 <?php
-    session_name("some_session_name");
-	session_set_cookie_params(0, '/', '.pobi.dom');
+    // session_name("some_session_name");
+	// session_set_cookie_params(0, '/', '.pobi.dom');
 
     //Demarrage de la sessions utilisateur
     session_start();
@@ -14,91 +14,152 @@
     require_once("../templates/functions.inc.php");
     require_once("../templates/navbar.php");
     require_once("../classes/password/passwordLib.php");
+    require_once("../classes/detectmobile/Mobile_Detect.php");
 
 
     use App\Service\MySqlConnection;
     use App\Service\RepertoireFonction;
     use App\Service\InformixConnection;
 
-    $connectionMySql = new MySqlConnection();
-    $connectionSystem = new InformixConnection("system");
-    $fonctions = new RepertoireFonction();
-    
-    if(empty($_SESSION["connecte"]) || $_SESSION["connecte"] !== true ) {
-        if(!empty($_POST["identifiant"])) {
-            //On explose l'identifiant en fonction de _
-            $tableauIdentifiant = explode("_",$_POST["identifiant"]);
+    $mySql = new MySqlConnection();
+    $bdd = $mySql->getConnectionStock();
+    $bddSystem = new InformixConnection("system");
 
-            //Si il y a bien quelque chose en deuxieme position du tableau
-			if(isset($tableauIdentifiant[1])){
-				$identifiant = trim($tableauIdentifiant[1]);
-			}else{
-				$identifiant = "";
-			}
-			
-			//On recupere le mot de passe
-			$mdpForm = $_POST["mdpForm"];
 
-            //On vérifie que l'identifiant existe bien
-			$connexion = $connectionSystem->prepare("SELECT COUNT(*) FROM adm_user WHERE nom_user = ?");
-            $connexion->execute(array(strtoupper($identifiant)));
-			$nbResult = intval($connexion->fetch(PDO::FETCH_COLUMN));
-
-            //Si il existe
-			if($nbResult != 0) {
-                //On recupere le mot de passe
-				$mdp = $connectionSystem->prepare("SELECT cns2, cns3, inactif FROM adm_user WHERE nom_user = ?");
-                $mdp->execute(array(strtoupper($identifiant)));
-				$mdpBdd = $mdp->fetch(PDO::FETCH_ASSOC);
-        
-                //On vérifie le mot de passe
-				if (password_verify($mdpForm, $mdpBdd["CNS2"].$mdpBdd["CNS3"]) && $mdpBdd["INACTIF"] != "désactivé") {
-                    $_SESSION["connecte"] = true;
-					$_SESSION["pseudo"] = trim(strtoupper($identifiant));
-
-                    $notif = $fonctions->notifier("Connexion réussie !");
-                    echo $notif;
-
-                } else {
-                    if($mdpBdd["INACTIF"] == "désactivé"){
-                        $erreur = $fonctions->notifier("Le compte est désactivé !");
-					} else {
-                        $erreur = $fonctions->notifier("Le couple login/mot de passe est incorrect !");
-					}
-                    echo $erreur;
-                }
-            } else {
-                $erreur = $fonctions->notifier("Identifiant incorrect");
-                echo $erreur;
-            }
-        }
-        echo 
-            '<div class="formulaire jumbotron col-6 center">' . 
-                '<h3>Connexion</h3>' . 
-                '<div class="form-group">' . 
-                    '<div class="input-prepend">' . 
-                        '<div class="input-group mb-2">' . 
-                            '<div class="input-group-prepend">' . 
-                                '<div class="input-group-text"><i class="fa fa-user"></i></div>' . 
-                            '</div>' . 
-                            '<input type="text" class="form-control" id="user-sylob" placeholder="Identifiant">' . 
-                        '</div>' . 
-                    '</div>' . 
-                '</div>' . 
-                '<div class="form-group">' . 
-                    '<div class="input-prepend">' . 
-                        '<div class="input-group mb-2">' . 
-                            '<div class="input-group-prepend">' . 
-                                '<div class="input-group-text"><i class="fa fa-key"></i></div>' . 
-                            '</div>' . 
-                            '<input type="password" class="form-control" id="mdp-sylob" placeholder="Mot de passe">' . 
-                        '</div>' . 
-                    '</div>' . 
-                '</div>' . 
-                '<input id="connexion-operateur" type="button" class="btn btn-primary" name="connexion" value="Connexion">' . 
-            '</div>' . 
-            '<br clear="all" />'
-        ;
-    } else {
-		header("Refresh:0;url=home.php");
+    //Recuperation user agent
+	$userAgent = $_SERVER['HTTP_USER_AGENT'];
+	
+	//Nou sexplosons celui-ci
+	$explodeUserAgent = explode("; ", $userAgent);
+	
+	//Si nous arrivons a recuperer le type de mobile
+    if(count($explodeUserAgent) >= 3) {
+		$mobileType = $explodeUserAgent[2];
+	} else {
+		$mobileType = "";
+	}
+	
+    $device = "other";
+	//Si le device comme par TC alors zebra
+    if(substr($mobileType, 0, 2) == "TC") {
+        $device = "zebra";
     }
+	
+	$detect = new Mobile_Detect;	
+	
+	//Si nous detectons que nous sommes sur un zebra nous lancons le login à vide comme firefox
+	if($detect->isMobile() == false && $device != "zebra"){
+		$login = connexionWindows();
+	}else{
+		$login = "";
+	}
+
+    //Si nous recuperons quelque chose dans le login --> test autoconnect	
+	if(!empty($login) && (!isset($_SESSION["connecte"]) || $_SESSION["connecte"] == false)) {
+		$ad = connexionAD($login);
+		if(!empty($ad)) {
+			$_SESSION["connecte"] = true;
+			$_SESSION["pseudo"] = $login;
+			//On regarde si la personne connectée fait partie de ceux pouvant voir le bouton administration
+			$testAdmin = $bdd->prepare("SELECT COUNT(*) FROM admin_appli WHERE login = ?");
+            $testAdmin->execute(array($login));
+			$nbTestAdmin = intval($testAdmin->fetch(PDO::FETCH_COLUMN));
+			if($nbTestAdmin != 0) {
+				$_SESSION["su"] = true;
+			} else {
+				$_SESSION["su"] = false;						
+			}
+		}
+	//Si nous avons envoyé le formulaire de connexion (zebra et firefox)	
+	} else if(isset($_POST["login"])) {
+		// On explose l'identifiant en fonction de _
+		$tableauIdentifiant = explode("_",$_POST["login"]);
+		// Si il y a bien quelque chose en deuxieme position du tableau
+		if(isset($tableauIdentifiant[1])) {
+			$identifiant = trim($tableauIdentifiant[1]);
+			// On recupere le mot de passe		
+			$mdpForm = $_POST["password"];
+			// On vérifie que l'identifiant existe bien
+			$connexion = $bddSystem->prepare("SELECT COUNT(*) FROM adm_user WHERE nom_user = ?");
+            $connexion->execute(array(trim(strtoupper($identifiant))));
+			$nbResult = intval($connexion->fetch(PDO::FETCH_COLUMN));
+			// Si il existe
+			if($nbResult != 0) {
+				// On recupere le mot de passe
+				$mdp = $bddSystem->prepare("SELECT cns2, cns3, inactif FROM adm_user WHERE nom_user =  ?");
+                $mdp->execute(array(trim(strtoupper($identifiant))));
+				$mdpBdd = $mdp->fetch(PDO::FETCH_ASSOC);
+				// On vérifie le mot de passe
+				if(password_verify($mdpForm, $mdpBdd["CNS2"].$mdpBdd["CNS3"]) && $mdpBdd["INACTIF"] != "désactivé") {
+					$_SESSION["connecte"] = true;
+					$_SESSION["pseudo"] = $identifiant;
+					//On regarde si la personne connecté fait partie de ceux pouvant voir le bouton administration
+					$testAdmin = $bdd->prepare("SELECT COUNT(*) FROM admin_appli WHERE login = ?");
+                    $testAdmin->execute(array($identifiant));
+					$nbTestAdmin = intval($testAdmin->fetch(PDO::FETCH_COLUMN));
+					if($nbTestAdmin != 0) {
+						$_SESSION["su"] = true;
+					} else {
+						$_SESSION["su"] = false;						
+					}
+				} else {
+					echo '<div class="alert alert-danger col-12 text-center mb-0" role="alert">Le mot de passe renseigné est incorrect.</div>';
+				}	
+			} else {
+				echo '<div class="alert alert-danger col-12 text-center mb-0" role="alert">Le login renseigné n\'existe pas dans la base de données.</div>';
+			}	
+		} else {
+			echo '<div class="alert alert-danger col-12 text-center mb-0" role="alert">Le login renseigné doit commencé par OPL_.</div>';
+		}	
+		
+	//Si nous arrivons a recupere le login windows
+	} else if(isset($_POST["logout"]) && $_POST["logout"] == "true") {
+		$_SESSION["connecte"] = false;
+		session_destroy();
+		echo '<div class="alert alert-success col-12 text-center mb-0" role="alert">Déconnexion réussie.</div>';	
+	}
+
+    //Formulaire des applis --> si nous sommes connecté
+	if(isset($_SESSION["connecte"]) && $_SESSION["connecte"] == true) {
+		$connecte = "true";
+        header("Refresh:0;url=home.php");
+	//Pas connecté --> formulaire de connexion	
+	} else {
+		$connecte = "false";
+		echo 
+            '<div class="row justify-content-center m-0" id="recherche">
+                <div class="justify-content-center mb-2 col-12">
+                    <span id="title" class="animate__animated  animate__backInDown inline-block">Formulaire de Connexion</span>
+                </div>		
+	    	</div>
+            <form id="formConnexion" method="POST" action="home.php" class="pt-3 pb-3 form-control">
+                <div class="row justify-content-center margin-auto">
+                    <div class="col-11 col-md-6">
+                        <label class="sr-only" for="login">Login</label>
+                        <div class="input-group mb-2">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><i class="fa fa-user"></i>&nbsp;</div>
+                            </div>
+                            <input type="text" name="login" class="form-control" id="login" placeholder="Login" required />
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center margin-auto">
+                    <div class="col-11 col-md-6">
+                        <label class="sr-only" for="password">Mot de passe</label>
+                        <div class="input-group mb-2">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><i class="fa fa-key"></i>&nbsp;</div>
+                            </div>
+                            <input type="number" name="password" max-length="6" class="form-control" id="password" placeholder="Mot de passe" required />
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center mt-3 margin-auto">
+                    <button type="submit" name="envoyer" value="true" class="btn btn-success col-10 col-md-3"><i class="fa fa-power-off"></i> Connexion</button>
+                </div>			
+            </form>'
+        ;
+	}	
+	echo '<br clear="all" />';
+    echo '</body></html>';
--- templates/functions.inc.php
+++ templates/functions.inc.php
@@ -1,43 +1,40 @@
 <?php
-
-		### SSO ###
+    
+	### SSO ###
 	
 	/*
-	 * Essaye de r�cup�rer le login windows
-	 * @return String Login windows si r�ussi, false si la tentative de r�cup�ration a �chou�
+	 * Essaye de récupérer le login windows
+	 * @return String Login windows si réussi, false si la tentative de récupération a échoué
 	 */
 	function connexionWindows(){
 		
 		$browser = getBrowser();
 		
-		// Si l'utilisateur n'utilise pas windows, ou si le navigateur n'est pas chrome ni IE, �choue
-		// if ($browser['platform'] != 'windows' || ( $browser['name'] != 'Google Chrome' && $browser['name'] != 'Internet Explorer')) {
-			// return false;
-		// }
-		
-	 
-		$headers = apache_request_headers(); // R�cup�ration des l'ent�tes client		
-	
-	 
-		if (@$_SERVER['HTTP_VIA'] != NULL) { // nous verifions si un proxy est utilis� : parceque l'identification par ntlm ne peut pas passer par un proxy
+		// Si l'utilisateur n'utilise pas windows, ou si le navigateur n'est pas chrome ni IE, échoue
+		if ($browser['platform'] != 'windows' || ( $browser['name'] != 'Google Chrome' && $browser['name'] != 'Internet Explorer')) {
 			return false;
-		} 
-		elseif (!isset($headers['Authorization'])) { //si l'entete autorisation est inexistante
+		}
+	 
+		$headers = apache_request_headers(); // Récupération des l'entêtes client
+	 
+		if (@$_SERVER['HTTP_VIA'] != NULL) { // nous verifions si un proxy est utilisé : parceque l'identification par ntlm ne peut pas passer par un proxy
+			return false;
+		} elseif (!isset($headers['Authorization'])) { //si l'entete autorisation est inexistante
 			header("HTTP/1.1 401 Unauthorized"); //envoi au client le mode d'identification
 			header("Connection: Keep-Alive");
 			header("WWW-Authenticate: Negotiate");
 			header("WWW-Authenticate: NTLM"); //dans notre cas le NTLM
 			exit;
 		}
-		
+	 
 		if (!isset($headers['Authorization'])) {
 			return false;
 		}
-		
+	 
 		if (substr($headers['Authorization'], 0, 5) != 'NTLM ') {
-			return false; // on v�rifie que le client soit en NTLM
+			return false; // on vérifie que le client soit en NTLM
 		}
-		
+	 
 		$chaine = $headers['Authorization'];
 		$chaine = substr($chaine, 5); // recuperation du base64-encoded type1 message
 		$chained64 = base64_decode($chaine); // decodage base64 dans $chained64
@@ -49,7 +46,7 @@
 			$retAuth64 = base64_encode($retAuth); // encode en base64
 			$retAuth64 = trim($retAuth64); // enleve les espaces de debut et de fin
 			header("HTTP/1.1 401 Unauthorized"); // envoi le nouveau header
-			header("WWW-Authenticate: NTLM $retAuth64"); // avec l'identification suppl�mentaire
+			header("WWW-Authenticate: NTLM $retAuth64"); // avec l'identification supplémentaire
 			exit;
 		} else if (ord($chained64{8}) == 3) {
 			$lenght_domain = (ord($chained64[31]) * 256 + ord($chained64[30])); // longueur du domain
@@ -63,13 +60,12 @@
 				return $login;
 			}
 		}
-		
 		return false;
 	}
 	
 	 
 	/**
-	 * R�cup�re le type de navigateur de l'utilisateur avec sa version, son 
+	 * Récupère le type de navigateur de l'utilisateur avec sa version, son 
 	 * @return array Tableau contenant des informations sur le navigateur de l'utilisateur
 	 */
 	function getBrowser() {
@@ -107,12 +103,12 @@
 	}
 	
 	
-	//Connexion � l'active directory qui remonte les infos
+	//Connexion à l'active directory qui remonte les infos
 	function connexionAD($login){
 		
-		// El�ments d'authentification LDAP
+		// Eléments d'authentification LDAP
 		$ldaprdn  = 'adru@pobi.dom';     // DN ou RDN LDAP
-		$ldappass = '@st2oo7!*';  // Mot de passe associ�
+		$ldappass = '@st2oo7!*';  // Mot de passe associé
 
 		// Connexion au serveur LDAP
 		// $ldapconn = ldap_connect("annuaire.pobi.dom")
@@ -127,7 +123,7 @@
 			// Connexion au serveur LDAP
 			$ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
 			
-			// V�rification de l'authentification
+			// Vérification de l'authentification
 			if($ldapbind == true){
 				
 				$filter = "(sAMAccountName=$login)";
@@ -146,158 +142,12 @@
 				}
 										
 			}elseif($ldapbind == false) {
-				// return "Connexion LDAP �chou�e...";
+				// return "Connexion LDAP échouée...";
 				return false;
 			}
 
 		}
 		
 	}
-    
-	function remove_accent($str) { // fonction cr��e par "hello at weblap dot ro" (commentaire sur fr2.php.net/preg_replace)
-				// utilis�e dans la fonction de recherche pour ne pas prendre en compte les accents et lettre doubles
-		// $a = array('�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', 
-		// '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', 
-		// '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', '�', 'A', 'a', 'A', 'a', 'A', 'a', 'C', 'c',
-		// 'C', 'c', 'C', 'c', 'C', 'c', 'D', 'd', '�', 'd', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'G', 'g', 'G', 'g', 'G',
-		// 'g', 'G', 'g', 'H', 'h', 'H', 'h', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', '?', '?', 'J', 'j', 'K', 'k',
-		// 'N', 'n', 'N', 'n', 'N', 'n', '?', 'O', 'o', 'O', 'o', 'O', 'o', '�', '�', 'R', 'r',
-		// 'R', 'r', 'R', 'r', 'S', 's', 'S', 's', 'S', 's', '�', '�', 'T', 't', 'T', 't', 'T', 't', 'U', 'u', 'U', 'u', 'U', 'u', 'U',
-		// 'u', 'U', 'u', 'U', 'u', 'W', 'w', 'Y', 'y', '�', 'Z', 'z', 'Z', 'z', '�', '�', '?', '�', 'O', 'o', 'U', 'u', 'A', 'a', 'I',
-		// 'i', 'O', 'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', '?', '?', '?', '?', '?', '?');
-		// $b = array('A', 'A', 'A', 'A', 'A', 'A', 'AE', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'D', 'N',
-		// 'O', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 's', 'a', 'a', 'a', 'a', 'a', 'a', 'ae', 'c', 'e', 'e', 'e', 'e',
-		// 'i', 'i', 'i', 'i', 'n', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', 'A', 'a', 'A', 'a', 'A', 'a', 'C', 'c',
-		// 'C', 'c', 'C', 'c', 'C', 'c', 'D', 'd', 'D', 'd', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'G', 'g', 'G', 'g', 'G',
-		// 'g', 'G', 'g', 'H', 'h', 'H', 'h', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'IJ', 'ij', 'J', 'j', 'K', 'k', 
-		// 'N', 'n', 'N', 'n', 'N', 'n', 'n', 'O', 'o', 'O', 'o', 'O', 'o', 'OE', 'oe', 'R', 'r',
-		// 'R', 'r', 'R', 'r', 'S', 's', 'S', 's', 'S', 's', 'S', 's', 'T', 't', 'T', 't', 'T', 't', 'U', 'u', 'U', 'u', 'U', 'u', 'U',
-		// 'u', 'U', 'u', 'U', 'u', 'W', 'w', 'Y', 'y', 'Y', 'Z', 'z', 'Z', 'z', 'Z', 'z', 's', 'f', 'O', 'o', 'U', 'u', 'A', 'a', 'I',
-		// 'i', 'O', 'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'A', 'a', 'AE', 'ae', 'O', 'o');
-		
-			
-		$unwanted_array = array('�'=>'S', '�'=>'s', '�'=>'Z', '�'=>'z', '�'=>'A', '�'=>'A', '�'=>'A', '�'=>'A', '�'=>'A', '�'=>'A', '�'=>'A', '�'=>'C', '�'=>'E', '�'=>'E',
-                            '�'=>'E', '�'=>'E', '�'=>'I', '�'=>'I', '�'=>'I', '�'=>'I', '�'=>'N', '�'=>'O', '�'=>'O', '�'=>'O', '�'=>'O', '�'=>'O', '�'=>'O', '�'=>'U',
-                            '�'=>'U', '�'=>'U', '�'=>'U', '�'=>'Y', '�'=>'B', '�'=>'Ss', '�'=>'a', '�'=>'a', '�'=>'a', '�'=>'a', '�'=>'a', '�'=>'a', '�'=>'a', '�'=>'c',
-                            '�'=>'e', '�'=>'e', '�'=>'e', '�'=>'e', '�'=>'i', '�'=>'i', '�'=>'i', '�'=>'i', '�'=>'o', '�'=>'n', '�'=>'o', '�'=>'o', '�'=>'o', '�'=>'o',
-                            '�'=>'o', '�'=>'o', '�'=>'u', '�'=>'u', '�'=>'u', '�'=>'y', '�'=>'b', '�'=>'y');
-		return strtr( $str, $unwanted_array );
-		
-		return str_replace($a, $b, $str);
-	}
 	
-	
-	//Fonction qui permet d'enlever les "-" et les "'" des insertions
-	function removeCaractere($string){
-		
-		$stringModi = str_replace("-"," ",$string);
-		$stringModi = str_replace("'"," ",$stringModi);
-		$stringModi = str_replace('"',' ',$stringModi);
-		$stringModi = str_replace('�',' ',$stringModi);
-		$stringModi = str_replace('/',' ',$stringModi);
-		$stringModi = str_replace('\\',' ',$stringModi);		
-		
-		return $stringModi;
-		
-	}
-	
-	function notifier($message, $bool_reussite = true, $fermer_la_boite = true)	{ /* C.Coulon, mai 2016 ; remplace la pr�c�dente fonction notification */
-
-		$boite_notif = '<div class="notif '; // classe notif de la notification, suivie de classe echec ou succes
-		if ($bool_reussite === false)
-			$boite_notif .= 'echec"><p><span style="color:darkred"><i class="fa fa-times"></i></span>';
-		else
-			$boite_notif .= 'succes"><p><span style="color:darkgreen"><i class="fa fa-check"></i></span>';
-		
-		$boite_notif .= '<br />'.$message.'</p>';
-
-		if ($fermer_la_boite === true) 
-			$boite_notif .= '</div><br /><br />';
-		
-		echo $boite_notif;
-	}
-
-	function pendingMessage($message) {
-		$boite_notif = '<div class="notif warning"><p><span style="color:darkred"><i class="fa fa-times"></i></span>';
-		$boite_notif .= '<br />'.$message.'</p>';
-
-		echo $boite_notif;
-	}
-	
-	// Fonction permettant d'afficher sur 5 caract�re les ID
-	function afficheId($id){
-		$nombre = strlen($id);
-		
-		switch($nombre){
-			case(1): $idRetour = '0000'.$id;
-				break;
-			case(2): $idRetour = '000'.$id;
-				break;
-			case(3): $idRetour = '00'.$id;
-				break;
-			case(4): $idRetour = '0'.$id;
-				break;
-			case(5): $idRetour = $id;
-				break;
-		}
-		
-		return $idRetour;
-	}
-	
-	//Fonction permettant d'afficher la date en francais
-	function affichedate($date){
-		
-		$date_creation = date("d/m/Y", strtotime($date));
-		$heure_creation = substr($date,10);
-		
-		
-		return $date_creation." � ".$heure_creation;
-	}
-	
-	//Fonction permettant d'afficher la date en francais
-	function afficheDateSeule($date){
-		
-		$date_creation = date("d/m/Y", strtotime($date));
-		$heure_creation = substr($date,10);
-		
-		
-		return $date_creation;
-	}
-	
-	//Fonction permettant de decrypter les coefs
-	function decrypter($maCleDeCryptage, $maChaineCrypter){
-		$maCleDeCryptage = md5($maCleDeCryptage);
-		$letter = -1;
-		$newstr = "";
-		$maChaineCrypter = base64_decode($maChaineCrypter);
-		$strlen = strlen($maChaineCrypter);
-		for ( $i = 0; $i < $strlen; $i++ ){
-			$letter++;
-			if ( $letter > 31 ){
-				$letter = 0;
-			}
-			$neword = ord($maChaineCrypter{$i}) - ord($maCleDeCryptage{$letter});
-			if ( $neword < 1 ){
-				$neword += 256;
-			}
-			$newstr .= chr($neword);
-		}
-		return $newstr;
-	}
-
-	function file_get_contents_curl($url) {
-		$ch = curl_init();
-	
-		curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);       
-	
-		$data = curl_exec($ch);
-		curl_close($ch);
-	
-		return $data;
-	}
-
- ?>
+?>
