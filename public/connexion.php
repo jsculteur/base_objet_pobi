7fe7e1006891390d423e1a1fea85508c73a774c1
jsculteur <julien.sculteur@pobi.fr>
2021-10-19 10:10:37 +0200

    Fix: Correction de la connection

--- public/connexion.php
+++ public/connexion.php
@@ -1,4 +1,6 @@
 <?php
+    session_name("some_session_name");
+	session_set_cookie_params(0, '/', '.pobi.dom');
 
     //Demarrage de la sessions utilisateur
     session_start();
@@ -15,127 +17,88 @@
 
 
     use App\Service\MySqlConnection;
+    use App\Service\RepertoireFonction;
     use App\Service\InformixConnection;
 
-    $connectionInformix = new InformixConnection("socpobi99_precix");
     $connectionMySql = new MySqlConnection();
-    $baseInformix = $connectionInformix->getConnection();
-    $baseUtilisateurs = $connectionMySql->getConnectionUtilisateurs();
-    $baseDG = $connectionMySql->getConnectionDG();
-    $login = connexionWindows();
-    $codeOutil = "BPSYLOB";
-    $sqlIdOutil = $baseUtilisateurs->prepare("SELECT idOutil FROM outil WHERE codeOutil = ?");
-    $sqlIdOutil->execute(array($codeOutil));
-    $idOutil = $sqlIdOutil->fetch(PDO::FETCH_COLUMN);
-    //Si le login n'est pas vide on regarde si il est dans l'active directory
-	if(!empty($login)) {
-		$ad = connexionAD($login);
-		$testLogin = "true";
-	} else {
-		$testLogin = "false";	
-	}
+    $connectionSystem = new InformixConnection("system");
+    $fonctions = new RepertoireFonction();
+    
+    if(empty($_SESSION["connecte"]) || $_SESSION["connecte"] !== true ) {
+        if(!empty($_POST["identifiant"])) {
+            //On explose l'identifiant en fonction de _
+            $tableauIdentifiant = explode("_",$_POST["identifiant"]);
 
-    if(empty($_SESSION["connecte"]) || $_SESSION["connecte"] !== true) {
-        if(!empty($_POST["identifiant_form"]) && !empty($_POST["mot_de_passe_form"])) {
-            $identifiant = trim($_POST["identifiant_form"]);
+            //Si il y a bien quelque chose en deuxieme position du tableau
+			if(isset($tableauIdentifiant[1])){
+				$identifiant = trim($tableauIdentifiant[1]);
+			}else{
+				$identifiant = "";
+			}
+			
+			//On recupere le mot de passe
+			$mdpForm = $_POST["mdpForm"];
 
-			$sqlConnection = $baseUtilisateurs->prepare("SELECT idUt, login, password, validite, idTypeUt FROM utilisateur WHERE login = ?");
-            $sqlConnection->execute(array($identifiant));
-			$resultConnexion = $sqlConnection->fetch(PDO::FETCH_ASSOC);
+            //On vérifie que l'identifiant existe bien
+			$connexion = $connectionSystem->prepare("SELECT COUNT(*) FROM adm_user WHERE nom_user = ?");
+            $connexion->execute(array(strtoupper($identifiant)));
+			$nbResult = intval($connexion->fetch(PDO::FETCH_COLUMN));
 
-            if(password_verify($_POST["mot_de_passe_form"], $resultConnexion["password"])) {
-                $_SESSION["connecte"] = true;
-				$_SESSION["idUt"] = $resultConnexion["idUt"];
-				$_SESSION["pseudo"] = $resultConnexion["login"];
+            //Si il existe
+			if($nbResult != 0) {
+                //On recupere le mot de passe
+				$mdp = $connectionSystem->prepare("SELECT cns2, cns3, inactif FROM adm_user WHERE nom_user = ?");
+                $mdp->execute(array(strtoupper($identifiant)));
+				$mdpBdd = $mdp->fetch(PDO::FETCH_ASSOC);
+        
+                //On vérifie le mot de passe
+				if (password_verify($mdpForm, $mdpBdd["CNS2"].$mdpBdd["CNS3"]) && $mdpBdd["INACTIF"] != "désactivé") {
+                    $_SESSION["connecte"] = true;
+					$_SESSION["pseudo"] = trim(strtoupper($identifiant));
 
-                //On regarde si il est su ou non
-				$sqlAdmin = $baseUtilisateurs->prepare("SELECT * FROM vue_administrateur WHERE Pseudo = ?");
-                $sqlAdmin->execute(array($_SESSION["pseudo"]));
-                $estAdmin = $sqlAdmin->fetch(PDO::FETCH_ASSOC);
-                if ($estAdmin === false) {
-					$_SESSION["su"] = false; // su pour Super User, soit administrateur.
-				} else {
-					if (!empty($estAdmin["Pseudo"]) && $estAdmin["Pseudo"] === $_SESSION["pseudo"]) { // ce qui est le cas d'après la requete même
-						$_SESSION["su"] = true;
+                    $notif = $fonctions->notifier("Connexion réussie !");
+                    echo $notif;
+
+                } else {
+                    if($mdpBdd["INACTIF"] == "désactivé"){
+                        $erreur = $fonctions->notifier("Le compte est désactivé !");
+					} else {
+                        $erreur = $fonctions->notifier("Le couple login/mot de passe est incorrect !");
 					}
-				}
-                notifier('Connexion réussie !');
-                // header("refresh:3;");
-                header("refresh:3;url=home.php");
-            }
-        } else if(!empty($login) && $testLogin == "true" && !isset($_POST["enregLoginAd"]) && empty($_POST["logout"])) {
-            $loginClause = strtolower($login);
-            $testUtilisateur = $baseUtilisateurs->prepare("SELECT * FROM utilisateur INNER JOIN tj_outil_ut ON tj_outil_ut.idUt = utilisateur.idUt	INNER JOIN outil ON tj_outil_ut.idOutil = outil.idOutil	WHERE login = ? AND codeOutil = ?");
-            $testUtilisateur->execute(array($loginClause, $codeOutil));
-
-			$nbRetour = $testUtilisateur->rowCount();
-            //Si la personne fait bien parti de notre base de données utilisateur alors on la connecte en automatique
-			if($nbRetour != 0) {
-                $resultConnexion = $testUtilisateur->fetch(PDO::FETCH_ASSOC);
-                $_SESSION["connecte"] = true;
-				$_SESSION["idUt"] = $resultConnexion["idUt"];
-				$_SESSION["pseudo"] = $resultConnexion["login"];
-				$_SESSION["codeOutil"] = $codeOutil;
-
-                //On regarde si il est su ou non
-				$sqlestAdmin = $baseUtilisateurs->prepare("SELECT * FROM vue_administrateur WHERE Pseudo = ?");
-                $sqlestAdmin->execute(array($_SESSION["pseudo"]));
-                $estAdmin = $sqlestAdmin->fetch(PDO::FETCH_ASSOC);
-				if ($estAdmin === false) {
-					$_SESSION["su"] = false; // su pour Super User, soit administrateur.
-				} else {
-					if (!empty($estAdmin["Pseudo"]) && $estAdmin["Pseudo"] === $_SESSION["pseudo"]) { // ce qui est le cas d'après la requete même
-						$_SESSION["su"] = true;
-					}
-				}
-                notifier('Connexion réussie !');
-                header("refresh:3;url=home.php");
+                    echo $erreur;
+                }
             } else {
-				notifier('Vous n\'avez pas accès à cet outil', false);
-			}
-        } else {
-            if(!empty($_POST["connexion"])) {
-				$message_erreur = 'Pour vous connecter :';
-				if (empty($_POST["identifiant_form"])) {
-					$message_erreur .= 'Veuillez préciser un identifiant. <br />';
-				}
-				if (empty($_POST["mot_de_passe_form"])) {
-					$message_erreur .= 'Veuillez préciser un mot de passe.';
-				}
-				notifier($message_erreur, false);
-			}
-            if(!isset($_SESSION["connecte"])) {
-                echo 
-                    '<form action="index.php" method="post">' . 
-                        '<div class="formulaire jumbotron col-6 center">' . 
-                            '<h1 class="display-4">Connexion</h1>' . 
-                            '<div class="form-group">' . 
-                                '<div class="input-prepend">' . 
-                                    '<div class="input-group mb-2">' . 
-                                        '<div class="input-group-prepend">' . 
-                                            '<div class="input-group-text"><i class="fa fa-user"></i></div>' . 
-                                        '</div>' . 
-                                        '<input type="text" class="form-control" name="identifiant_form" id="identifiant_form" placeholder="Identifiant">' . 
-                                    '</div>' . 
-                                '</div>' . 
-                            '</div>' . 
-                            '<div class="form-group">' . 
-                                '<div class="input-prepend">' . 
-                                    '<div class="input-group mb-2">' . 
-                                        '<div class="input-group-prepend">' . 
-                                            '<div class="input-group-text"><i class="fa fa-key"></i></div>' . 
-                                        '</div>' . 
-                                        '<input type="password" class="form-control" id="mot_de_passe_form" name="mot_de_passe_form" placeholder="Mot de passe">' . 
-                                    '</div>' . 
-                                '</div>' . 
-                            '</div>' . 
-                            '<input type="submit" class="btn btn-primary" name="connexion" value="Connexion">' . 
-                        '</div>' . 
-                    '</form>' . 
-                    '<br clear="all" />'
-                ;
+                $erreur = $fonctions->notifier("Identifiant incorrect");
+                echo $erreur;
             }
         }
+        echo 
+            '<div class="formulaire jumbotron col-6 center">' . 
+                '<h3>Connexion</h3>' . 
+                '<div class="form-group">' . 
+                    '<div class="input-prepend">' . 
+                        '<div class="input-group mb-2">' . 
+                            '<div class="input-group-prepend">' . 
+                                '<div class="input-group-text"><i class="fa fa-user"></i></div>' . 
+                            '</div>' . 
+                            '<input type="text" class="form-control" id="user-sylob" placeholder="Identifiant">' . 
+                        '</div>' . 
+                    '</div>' . 
+                '</div>' . 
+                '<div class="form-group">' . 
+                    '<div class="input-prepend">' . 
+                        '<div class="input-group mb-2">' . 
+                            '<div class="input-group-prepend">' . 
+                                '<div class="input-group-text"><i class="fa fa-key"></i></div>' . 
+                            '</div>' . 
+                            '<input type="password" class="form-control" id="mdp-sylob" placeholder="Mot de passe">' . 
+                        '</div>' . 
+                    '</div>' . 
+                '</div>' . 
+                '<input id="connexion-operateur" type="button" class="btn btn-primary" name="connexion" value="Connexion">' . 
+            '</div>' . 
+            '<br clear="all" />'
+        ;
     } else {
-        header("refresh:0;url=home.php");
+		header("Refresh:0;url=home.php");
     }
--- public/home.php
+++ public/home.php
@@ -4,4 +4,6 @@
     $titrepage = "Nom Page";
     require_once("../vendor/autoload.php");
     require_once("../templates/navbar.php");
-    
+
+    // Rediriger vers votre page
+    // header("Refresh:0;url=chemin_vers_ma_page.php");
